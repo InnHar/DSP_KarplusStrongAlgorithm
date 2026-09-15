8a578a51485cad673857e52e7e7e9bb777ea89f4
Inna <innnhar@gmail.com>
2026-09-15 17:13:57 +0400

    Generated sinusoidal data of 440 Hz frequency

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,8 +6,9 @@ root_path = Path.cwd()
 
 audiofile_dir = root_path / "AudioFiles"
 audiofile_path =  audiofile_dir / "v5.wav"
-audiofile_greater_volume = audiofile_dir / "twice_greater_volume.wav"
-audio_low_sr = audiofile_dir / "audio_low_sr.wav"
+audiofile_greater_volume_path = audiofile_dir / "twice_greater_volume.wav"
+audio_low_sr_path = audiofile_dir / "audio_low_sr.wav"
+sinusoidal_path = audiofile_dir / "sinusoid.wav"
 
 print(f"The wave file path: {audiofile_path}")
 
@@ -20,11 +21,15 @@ print("Sampling rate: ", sr)
 # plot_waveform(audio_data, output_dir="plots", filename="v5_waveform.png")
 # plot_waveform(audio_data[50000:100000], output_dir="plots", filename="v5_waveform_segment.png")
 
-print("Writing new wav with 2 times greater volume. Saved in ", audiofile_greater_volume)
+print("Writing new wav with 2 times greater volume. Saved in ", audiofile_greater_volume_path)
 louder_data = audio_data * 2.0
 if np.max(np.abs(louder_data)) > 32767:
     louder_data = np.clip(louder_data, -32768, 32767)
-write_audio(audiofile_greater_volume, sr, louder_data)
+write_audio(audiofile_greater_volume_path, sr, louder_data)
 
-print("Decrease the audio sampling rate 2 times. Saved in ", audio_low_sr)
-change_sampling_rate(audiofile_path, audio_low_sr, sr//2)
+print("Decrease the audio sampling rate 2 times. Saved in ", audio_low_sr_path)
+change_sampling_rate(audiofile_path, audio_low_sr_path, sr//2)
+
+generate_sinusoidal_signal(440, 5, sinusoidal_path)
+_, sinusoidal_data = read_audio(sinusoidal_path)
+plot_waveform(sinusoidal_data, output_dir="plots", filename="sinusoid.png")
