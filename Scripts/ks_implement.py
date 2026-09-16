3b0f30eb1fc40bda30eb154fda2dc18c6d518110
Inna <innnhar@gmail.com>
2026-09-16 12:33:19 +0400

    Implemented the Karplus Strong algorithm and applied

--- Scripts/ks_implement.py
+++ Scripts/ks_implement.py
@@ -5,3 +5,29 @@
 # 2) you have to create function which generates random x vector, feeds it to previous function
 # and saves y[n] output signal as .wav file
 # 3) Using last function you have to create wav examples
+import numpy as np
+from simple_wav import *
+
+def karplus_strong(x, a, N):
+    assert 0 < a < 1, 'a must be strictly btw 0 and 1'
+    M = len(x)
+    y = np.zeros(N, dtype=np.float32)
+    for i in range(N):
+        x_val = x[i] if i < M else 0.0
+        y_val = y[i - M] if i >= M else 0.0
+        y[i] = x_val + a * y_val
+    return y
+
+def generate_and_save_pluck(filename, freq=440, sr=44100, a=0.6, duration=3):
+    M = int(np.round(sr / freq))
+    x = np.random.uniform(-1.0, 1.0, M).astype(np.float32)
+    
+    N = int(duration * sr)
+    y = karplus_strong(x, a, N)
+
+    data = (y * 32767).astype(np.int16)
+
+    print("Saving the pluck at:", filename)
+    write_audio(filename, sr, data)
+
+    return y
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,4 +1,5 @@
 from simple_wav import *
+from ks_implement import *
 from pathlib import Path
 import os
 
@@ -10,26 +11,35 @@ audiofile_greater_volume_path = audiofile_dir / "twice_greater_volume.wav"
 audio_low_sr_path = audiofile_dir / "audio_low_sr.wav"
 sinusoidal_path = audiofile_dir / "sinusoid.wav"
 
-print(f"The wave file path: {audiofile_path}")
+# print(f"The wave file path: {audiofile_path}")
 
-sr, audio_data = read_audio(audiofile_path)
+# sr, audio_data = read_audio(audiofile_path)
 
-print(f"Power: {compute_power(audio_data)}")
+# print(f"Power: {compute_power(audio_data)}")
 
-print("Sampling rate: ", sr)
+# print("Sampling rate: ", sr)
 
-plot_waveform(audio_data, output_dir="Plots", filename="v5_waveform.png")
-plot_waveform(audio_data[50000:100000], output_dir="Plots", filename="v5_waveform_segment.png")
+# plot_waveform(audio_data, output_dir="Plots", filename="v5_waveform.png")
+# plot_waveform(audio_data[50000:100000], output_dir="Plots", filename="v5_waveform_segment.png")
 
-print("Writing new wav with 2 times greater volume. Saved in ", audiofile_greater_volume_path)
-louder_data = audio_data * 2.0
-if np.max(np.abs(louder_data)) > 32767:
-    louder_data = np.clip(louder_data, -32768, 32767)
-write_audio(audiofile_greater_volume_path, sr, louder_data)
+# print("Writing new wav with 2 times greater volume. Saved in ", audiofile_greater_volume_path)
+# louder_data = audio_data * 2.0
+# if np.max(np.abs(louder_data)) > 32767:
+#     louder_data = np.clip(louder_data, -32768, 32767)
+# write_audio(audiofile_greater_volume_path, sr, louder_data)
 
-print("Decrease the audio sampling rate 2 times. Saved in ", audio_low_sr_path)
-change_sampling_rate(audiofile_path, audio_low_sr_path, sr//2)
+# print("Decrease the audio sampling rate 2 times. Saved in ", audio_low_sr_path)
+# change_sampling_rate(audiofile_path, audio_low_sr_path, sr//2)
 
-generate_sinusoidal_signal(440, 5, sinusoidal_path)
-_, sinusoidal_data = read_audio(sinusoidal_path)
-plot_waveform(sinusoidal_data, output_dir="Plots", filename="sinusoid.png")
+# generate_sinusoidal_signal(440, 5, sinusoidal_path)
+# _, sinusoidal_data = read_audio(sinusoidal_path)
+# plot_waveform(sinusoidal_data, output_dir="Plots", filename="sinusoid.png")
+
+freq = 440
+sr = 44100
+M = int(np.round(sr / freq))
+
+y = generate_and_save_pluck(filename=audiofile_dir / "y_A4.wav", freq=440.0, a=0.99)
+# generate_and_save_pluck(filename=audiofile_dir / "y_C4.wav", freq=261.63)
+# generate_and_save_pluck(filename=audiofile_dir / "y_A2.wav", freq=110.0)
+
